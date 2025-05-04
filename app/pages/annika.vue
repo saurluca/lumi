654db109c2c418c4341d036bf1f5b08c62f5bad5
saurluca <spuckteller@gmail.com>
2025-05-04 19:45:20 +0200

    added page for annika

---
+++ app/pages/annika.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+const count = ref(0);
+
+function increment() {
+    count.value++;
+}
+
+function decrement() {
+    count.value--;
+}
+
+function reset() {
+    count.value = 0;
+}   
+
+</script>
+
+<template>
+  <div class="text-white w-screen h-screen flex justify-center items-center flex-col">
+    <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" @click="increment">
+      Increment
+    </button>
+    <div class="text-4xl">{{ count }}</div>
+    <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" @click="decrement">
+      Decrement
+    </button>
+    <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" @click="reset">
+      Reset
+    </button>
+  </div>
+</template>
